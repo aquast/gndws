89e08d1b8cf59d45c7373701118ed8ee01623b6f
Andres Quast <quast@hbz-nrw.de>
2013-04-24 16:44:26 +0200

    worked on opus js

--- src/main/webapp/js/pndId4OPUS3x.js
+++ src/main/webapp/js/pndId4OPUS3x.js
@@ -10,21 +10,23 @@ $(document).ready(addPndForm);
 
 var pRow;
 
-//add a PersonID-field to the form
+//add a PersonID-Input field to the form
 function addPndForm(){
 	var suchImage = "<div class=\"pnd\" style=\"margin-top:4px;\"><img src=\"../Icons/search_icon.jpg\" alt=\"PND nach Person durchsuchen\" style=\"float:left;\" />"
 	+ "<input class=\"pndid\" type=\"text\" size=\"30\" value=\"PND-ID\" /></div>";
 	var imageDiv = $(".person").parent().append(suchImage);
-	//$(".lastName").blur(requestPersonIdBlur);
 	$("head").append("<link  rel=\"stylesheet\" type=\"text/css\" href=\"/css/loddiggr.css\"/>");
 	$("img").click(requestPersonId);
-	$("body").append("<div class=\"result\"><h4>Ergebnis der PND ID-Abfrage</h4></div>");
+	$("body").append("<div class=\"result\"><div class=\"close\"><img src=\"../Icons/close-icon.png\" /></div><h4>Ergebnis der PND ID-Abfrage</h4></div>");
+	$(".person").blur(requestPersonIdBlur);
 	$(".result").hide();
+	$(".close").click(function(){$(".result").slideUp("slow");});
 
 };
 
-
+// create the remote PND request
 function requestPersonId(){
+    
     $(".resultalert").remove();	
     $(".result").hide();
     $(".item").remove();
@@ -36,9 +38,9 @@ function requestPersonId(){
 	pRow = $(this).parent().parent().parent();
 	if(pndFormCheck(firstName, lastName)){
 		requestGndService(firstName, lastName); 	
-		$(this).parent().parent().find(".pnd").append("<div class=\"resultalert\"></div>");
-		//$(this).parent().parent().find("img").remove();
-    	}
+		//$(this).parent().parent().find(".pnd").append("<div class=\"resultalert\"></div>");
+		//$(this).parent().parent().find(".pnd img").remove();
+	}
 };
 
 function requestPersonIdBlur(){
@@ -46,18 +48,20 @@ function requestPersonIdBlur(){
     $(".result").hide();
     $(".item").remove();
 
-    //$(this).parent().parent().find(".lastName").unbind("blur");
+    $(this).parent().parent().find(".person").unbind("blur");
 	
-	var firstName = $(this).parent().parent().find(".firstName").val();
-	var lastName = $(this).parent().parent().find(".lastName").val();
+	var personName = $(this).parent().parent().find(".person").val().split(",", 2);
+	
+	var firstName = personName[1].replace(" ", "");
+	var lastName = personName[0].replace(" ", "");
 
 	pRow = $(this).parent().parent();
 	if(pndFormCheck(firstName, lastName)){
-		requestGndService(firstName, lastName); 	
+		backgroundRequestGndService(firstName, lastName); 	
 		$(this).parent().parent().find(".pnd").append("<div class=\"resultalert\"></div>");
-		//$(this).parent().parent().find("img").remove();
     	}
 };
+
 
 function pndFormCheck(firstName, lastName){
     if(lastName == null || lastName == "" || firstName == null || firstName == ""){
@@ -67,9 +71,9 @@ function pndFormCheck(firstName, lastName){
     return true;
 }
 
+// perform remote request
 function requestGndService(firstName, lastName){
-	//return $("<p>" + lastName + ", " + firstName + "</p>");
-	var requestUrl = "http://phacops.dyndns.org:8080/axis2/services/gndRequester/getGndPersonInfo?firstName=" 
+	var requestUrl = "http://nyx.hbz-nrw.de/loddiggr/gndRequester/getGndPersonInfo?firstName=" 
 	+ firstName + "&lastName=" + lastName;
 	var options = {
 			
@@ -81,18 +85,22 @@ function requestGndService(firstName, lastName){
      var testText;
      var jqxhr = jQuery.ajax(options)
 			.done(function(){
-				$("div.resultalert").append($(jqxhr.responseText).find("resultSize").text());
-				$("div.resultalert").slideDown("slow");
-				$("div.resultalert").click(function(){
-					$(".result").show();
-					});
+				//$("div.resultalert").append("Hinweis: Es gibt <strong>" + $(jqxhr.responseText).find("resultSize").text() + " PND ID</strong> für diesen Namen");
+				//$("div.resultalert").slideDown("slow");
+				$(".result").slideDown("slow");
+
+				// make resultalert click able:
+				//$("div.resultalert").click(function(){
+				//	$(".result").show();
+				//	});
 				$("div.result").append(responseParser(jqxhr.responseText));
 				$("a.item strong").click(function(){
 					$(this).parent().parent().find("ul li strong").remove();
 					var pnd = $(this).parent().parent().find("ul li:first-child").text();
-					//var firstName = $(this).parent().parent().find("ul li:first-child").text();
-					pRow.find("input.PNDIdentNumber").val(pnd);
-					$(".result").hide();
+					pRow.find("input.pndid").val(pnd);
+					$(".result").slideUp("slow");
+				    $(".resultalert").slideUp("slow");	
+					
 				});
 			})
 			.fail(function(){
@@ -105,20 +113,70 @@ function requestGndService(firstName, lastName){
 
 }
 
+//perform remote request
+function backgroundRequestGndService(firstName, lastName){
+	var requestUrl = "http://nyx.hbz-nrw.de/loddiggr/gndRequester/getGndPersonInfo?firstName=" 
+	+ firstName + "&lastName=" + lastName;
+	var options = {
+			
+			type: 'GET',
+			url: requestUrl,
+			dataType: 'xml',
+            	}
+            
+     var testText;
+     var jqxhr = jQuery.ajax(options)
+			.done(function(){
+				$("div.resultalert").append("Hinweis: Es gibt <strong>" + $(jqxhr.responseText).find("resultSize").text() + " PND ID</strong> für diesen Namen");
+				$("div.resultalert").slideDown("slow");
+				
+				// make resultalert click able:
+				$("div.resultalert").click(function(){
+					$(".result").slideToggle("slow");
+					});
+				
+				$("div.result").append(responseParser(jqxhr.responseText));
+				$("a.item strong").click(function(){
+					$(this).parent().parent().find("ul li strong").remove();
+					var pnd = $(this).parent().parent().find("ul li:first-child").text();
+					pRow.find("input.pndid").val(pnd);
+					$(".result").slideUp("slow");
+				    $(".resultalert").slideUp("slow");	
+				});
+			})
+			.fail(function(){
+				alert("request failed: " + jqxhr.statusText);
+				})
+			.always(function(){
+			});
+    return jqxhr.responseText;
+            	
+
+}
+
+
 function responseParser(xml){
 	var resultField = "";
 	var pndResult = $(xml).find("result");
 	pndResult.each(function(){
 		var prefferedName = $(this).find("prefferedName").text();
+		var personName = prefferedName.split(",", 2);
+		
+		var firstName = personName[1].replace(" ", "");
+		var lastName = personName[0].replace(" ", "");
+		
 		var pndUri =$(this).find("pndUri").text();
 		var pndId = $(this).find("pndID").text();
 		var biogr = $(this).find("biograficData").text();
 		var birth =$(this).find("yearOfBirth").text();
 		
-		resultField = resultField + "<div class=\"item\" ><a href=\"#\" class=\"item\" ><strong>" + prefferedName + "</strong></a>" 
-			+ "<ul><li><strong>PND-ID: </strong>" +  pndId + "</li>"
+		resultField = resultField + "<div class=\"item\" ><a href=\"#\" class=\"item\" ><strong>Treffer</strong></a>" 
+			+ "<ul><li><strong>PND-ID: </strong>" +  pndId +"</li>"
+			+ "<li><strong>Name: </strong>" +  firstName + " " + lastName +"</li>"
 			+ "<li><strong>Geburtsjahr: </strong>" + birth + "</li>"
-			+ "<li><strong>Bibliographische Daten: </strong>" + biogr  + "</li></ul></div>";
+			+ "<li><strong>Bibliographische Daten: </strong>" + biogr  + "</li>" 
+			+ "<li><strong>DNB-Eintrag: </strong><a href=\"" + pndUri + "\" target=\"_blamk\" >"+ pndUri  + "</a></li>" 
+			+ "</li></ul></div>";
 	});
 	return resultField;
 }
